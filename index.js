5935f2ec00102e25b287b73a0cbc8c6e3dc71b6c
NhatPhi1804 <phijerry180403@gmail.com>
2023-12-07 21:12:06 +0700

    publish

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+const expressHbs = require("express-handlebars");
+
+const cookiesParser = require('cookie-parser');
+const session = require('express-session');
+
+// Thiet lap thu muc Static
+app.use(express.static(__dirname + "/html"));
+
+app.use(cookiesParser('COOKIE_SECRET'));
+app.use(
+  session({
+    secret: "SESSION_SECRET",
+    resave: false,
+    saveUninitialized: false,
+    cookie:{
+      secure: false,
+      httpOnly: true,
+      maxAge: 20 * 60 * 1000,
+    }
+  })
+)
+
+
+// Cau hinh Template Engine
+app.engine(
+  "hbs",
+  expressHbs.engine({
+    layoutsDir: __dirname + "/views/layouts",
+    defaultLayout: "layout",
+    extname: "hbs",
+    runtimeOptions: {
+      allowProtoPropertiesByDefault: true,
+    },
+  })
+);
+app.set("view engine", "hbs");
+
+// Cau hinh cho phep doc du lieu gui len bang phuong thuc POST
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Chuyen huong route xu ly
+app.use("/", require("./routes/authRouter"));
+
+
+// Start web server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
